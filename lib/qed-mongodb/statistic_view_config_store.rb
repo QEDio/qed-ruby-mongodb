9953d49e830881c299f22ad21f65cc552bca24ef
Johannes A Kaefer <jak4@gmx.net>
2011-07-26 10:14:59 +0200

    always to_sym before trying to get from hash

--- lib/qed-mongodb/statistic_view_config_store.rb
+++ lib/qed-mongodb/statistic_view_config_store.rb
@@ -5,13 +5,13 @@ module Qed
           :kp => {
             :conversion_by_channel            => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBC_2 ],
             :conversion_by_product            => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBP_2 ],
-            :topseller                        => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBP_2 ]
+            :topseller                        => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBP_2 ],
             :tracking                         => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_TRA_2 ]
           }
         }
 
-      def self.get_config(user, action)
-        PROFILE[user][action]
+      def self.get_config(user, view)
+        PROFILE[user.to_sym][view.to_sym]
       end
     end
   end
