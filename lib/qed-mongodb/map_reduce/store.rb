bc3847f72a78b8b3e3d4e04e4753db28cbbd7aa0
Hans Hasselberg <hans.hasselberg@googlemail.com>
2011-07-26 09:09:52 +0200

    add map, reduce and finalize for tracking export

--- lib/qed-mongodb/map_reduce/store.rb
+++ lib/qed-mongodb/map_reduce/store.rb
@@ -70,6 +70,44 @@ module Qed
         KP_CBP_FINALIZE2=
               <<-JS
               JS
+
+        KP_TRA_MAP2 =
+              <<-JS
+                worked_on = 1;
+                qualified = 1;
+                test = 0;
+                count = 1;
+                turnover = value.turnover;
+                payed = value.payed;
+
+                if(value.status_id == 0){worked_on=0};
+                if(value.status_id == 4){test=1};
+                if(value.status_id != 1){qualified=0};
+              JS
+
+        KP_TRA_REDUCE2 =
+              <<-JS
+                var count = 0;
+                var worked_on = 0;
+                var qualified = 0;
+                var test = 0;
+                var turnover = 0;
+                var payed = 0;
+
+                values.forEach(function(v){
+                  count += v.count;
+                  worked_on += v.worked_on;
+                  qualified += v.qualified;
+                  test += v.test;
+                  turnover += v.turnover;
+                  payed += v.payed
+                });
+              JS
+
+        KP_TRA_FINALIZE2=
+              <<-JS
+              JS
+
       end
     end
   end
