4ca0798edbeb0c27142e49ac6a6841904bdd203a
Hans Hasselberg <hans.hasselberg@googlemail.com>
2011-07-26 09:10:06 +0200

    add tracking view

--- lib/qed-mongodb/statistic_view_config_store.rb
+++ lib/qed-mongodb/statistic_view_config_store.rb
@@ -6,6 +6,7 @@ module Qed
             :conversion_by_channel            => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBC_2 ],
             :conversion_by_product            => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBP_2 ],
             :topseller                        => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBP_2 ]
+            :tracking                         => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_TRA_2 ]
           }
         }
 
