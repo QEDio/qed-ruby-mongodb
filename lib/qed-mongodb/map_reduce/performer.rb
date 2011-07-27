121801a4bdcef70960a88e3058c9a390b7fbec2b
Johannes A Kaefer <jak4@gmx.net>
2011-07-28 00:01:40 +0200

    mapreduce calls are now depth-unlimited, basically we now can daisy-chain mapreduce calls as we wish

--- lib/qed-mongodb/map_reduce/performer.rb
+++ lib/qed-mongodb/map_reduce/performer.rb
@@ -6,65 +6,77 @@ module Qed
         MONGO_PORT = 27017
 
         def self.mapreduce(filter_model)
-          # goes out to the api
-          # the second mapreduce goes out to the api as well, in one single api call
-          mapreduce1(filter_model)
-          mapreduce2(filter_model)
+          init(filter_model)
+          int_mapreduce
         end
 
-        def self.conversion_by_channel_drilldown1(filter_model)
-          # this should be a query on the mapreduced table from mapreduce2
-          # goes out to the api
-          mapreduce1(filter_model)
-        end
-
-        # API
-        def self.atomic(filter_model)
-          filter_model.mongoid_condition(Qed::Mongodb::MongoidModel)
-        end
-
-        # API
-        def self.mapreduce1(filter_model)
-          init(filter_model, 0)
-          int_mapreduce()
-        end
-
-        # API
-        def self.mapreduce2(filter_model)
-          init(filter_model, 1)
-          int_mapreduce()
-        end
+        #def self.conversion_by_channel_drilldown1(filter_model)
+        #  # this should be a query on the mapreduced table from mapreduce2
+        #  mapreduce1(filter_model)
+        #end
+        #
+        ## API
+        #def self.atomic(filter_model)
+        #  filter_model.mongoid_condition(Qed::Mongodb::MongoidModel)
+        #end
+        #
+        ## API
+        #def self.mapreduce1(filter_model)
+        #  init(filter_model, 0)
+        #  int_mapreduce()
+        #end
+        #
+        ## API
+        #def self.mapreduce2(filter_model)
+        #  init(filter_model, 1)
+        #  int_mapreduce()
+        #end
 
 
         private
-          def self.init(filter_model, level)
+          def self.init(filter_model)
             raise Qed::Mongodb::Exceptions::OptionMisformed.new("Provided filter is not a FilterModel-Object!") unless filter_model.is_a?(Qed::Mongodb::FilterModel)
 
             @filter_model = filter_model
-            @mrm = Qed::Mongodb::StatisticViewConfig.create_config(@filter_model, level)
+            # @mrm is an array, containing the configuration for all necessary mapreduces
+            @mapreduce_models = Qed::Mongodb::StatisticViewConfig.create_config(@filter_model)
+
+            raise MapReduceConfigurationNotFound.new("Couldn't find any mapreduce configuration for this request.") if @mapreduce_models.size == 0
 
             # security, use only correct collection/db-names, a little bit weired, gerade ziehen
-            set_filter_model
+            #set_filter_model
 
-            db = Mongo::Connection.new(MONGO_HOST, MONGO_PORT).db(@filter_model.mongodb[:database])
-            @mr_collection = @filter_model.mongodb[:mr_collection]
-            @collection = db.collection(@filter_model.mongodb[:base_collection])
+            mongodb = @mapreduce_models[0].database
+
+            @db = Mongo::Connection.new(MONGO_HOST, MONGO_PORT).db(mongodb)
+
           end
 
-          def self.set_filter_model
-            @filter_model.replace_mongodb(
-                {
-                  :base_collection  => @mrm.base_collection,
-                  :mr_collection    => @mrm.mr_collection,
-                  :database         => @mrm.database
-                }
-            )
-            return @filter_model
-          end
+          #def self.set_filter_model
+          #  @filter_model.replace_mongodb(
+          #      {
+          #        :base_collection  => @mrm.base_collection,
+          #        :mr_collection    => @mrm.mr_collection,
+          #        :database         => @mrm.database
+          #      }
+          #  )
+          #  return @filter_model
+          #end
 
           # returns data, therefore from API
           def self.int_mapreduce
-            @collection.map_reduce(@mrm.map, @mrm.reduce, {:query => @mrm.query, :out => {:replace => @mr_collection }, :finalize => @mrm.finalize})
+            mr_cursor = nil
+
+            if @mapreduce_models.size == 1 && @mapreduce_models[0].query_only?
+              mr_cursor = @mapreduce_models[0].query
+            else
+              @mapreduce_models.each do |mrm|
+                collection = @db.collection(mrm.base_collection)
+                mr_cursor = collection.map_reduce(mrm.map, mrm.reduce, {:query => mrm.query, :out => {:replace => mrm.mr_collection }, :finalize => mrm.finalize})
+              end
+            end
+
+            return mr_cursor
           end
 
           ## make sure we have symbols as keys, not strings
