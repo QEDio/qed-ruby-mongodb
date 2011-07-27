c3a6a3b857aa9a250729b81863e9947e1db1b5a0
Johannes A Kaefer <jak4@gmx.net>
2011-07-27 23:59:55 +0200

    need more configuration data for map-reduces, prepared for that

--- lib/qed-mongodb/statistic_view_config_store.rb
+++ lib/qed-mongodb/statistic_view_config_store.rb
@@ -3,10 +3,34 @@ module Qed
     class StatisticViewConfigStore
       PROFILE = {
           :kp => {
-            :conversion_by_channel            => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBC_2 ],
-            :conversion_by_product            => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBP_2 ],
-            :topseller                        => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_CBP_2 ],
-            :tracking                         => [ Qed::Mongodb::MapReduce::Config::KP_CBP_1, Qed::Mongodb::MapReduce::Config::KP_TRA_2 ]
+            :conversion_by_channel            =>  {
+              # the order is important here, the first mapreduce will be done first, then the second and so on
+              :mapreduce =>       [
+                                      Qed::Mongodb::MapReduce::Config::KP_CBP_1,
+                                      Qed::Mongodb::MapReduce::Config::KP_CBC_2
+                                  ]
+            },
+
+            :conversion_by_product            =>  {
+                :mapreduce =>     [
+                                      Qed::Mongodb::MapReduce::Config::KP_CBP_1,
+                                      Qed::Mongodb::MapReduce::Config::KP_CBP_2
+                                  ]
+            },
+
+            :topseller                        =>  {
+              :mapreduce =>       [
+                                      Qed::Mongodb::MapReduce::Config::KP_CBP_1,
+                                      Qed::Mongodb::MapReduce::Config::KP_CBP_2
+                                  ]
+            },
+
+            :tracking                         =>  {
+              :mapreduce =>       [
+                                      Qed::Mongodb::MapReduce::Config::KP_CBP_1,
+                                      Qed::Mongodb::MapReduce::Config::KP_TRA_2
+                                  ]
+              }
           }
         }
 
