68fc69a2e66196208f160cd7cb70262e50d98183
Johannes A Kaefer <jak4@gmx.net>
2011-07-28 00:00:49 +0200

    now returns builder objects for all mapreduces necessary to build the request stats-view

--- lib/qed-mongodb/statistic_view_config.rb
+++ lib/qed-mongodb/statistic_view_config.rb
@@ -1,10 +1,32 @@
 module Qed
   module Mongodb
     class StatisticViewConfig
-      def self.create_config(fm, level)
-        config = Qed::Mongodb::StatisticViewConfigStore.get_config(fm.user, fm.view)[level]
-        config[:query] = fm.mongodb_query
-        Qed::Mongodb::MapReduce::Builder.new(config)
+      # from the config create the necessary objects to perform all mapreduce operations which are needed to generate
+      # the desired view/statistic
+      def self.create_config(filter_model)
+        # get the mapreduce-configurations
+        mapreduce_configurations = Qed::Mongodb::StatisticViewConfigStore.get_config(filter_model.user, filter_model.view)[:mapreduce]
+
+        if( mapreduce_configurations.size > filter_model.drilldown_level_current)
+          # only get those needed for the current drilldown level
+          # drilldown level == 0 is most reduced stats view
+          # drilldown level == size of returned configuration is unreduced stats view (but almost certainly filtered!)
+          mapreduce_configurations = mapreduce_configurations[0..mapreduce_configurations.size-(1+filter_model.drilldown_level_current)]
+          puts mapreduce_configurations.inspect
+
+          [].tap do |arr|
+            mapreduce_configurations.each_with_index do |config, i|
+              # only first mapreduce needs this filter query
+              config[:query] = i == 0 ? filter_model.mongodb_query : nil
+
+              arr << Qed::Mongodb::MapReduce::Builder.new(config)
+            end
+          end
+        # don't mapreduce, just show the filtered data
+        else
+          b = Qed::Mongodb::MapReduce::Builder.new({:query => filter_model.mongodb_query })
+          b.database = mapreduce_configurations[0][:database]
+        end
       end
     end
   end
