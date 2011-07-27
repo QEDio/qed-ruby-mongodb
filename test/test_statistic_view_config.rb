f84e60d13bc31381acd459da42a646a8559deb52
Johannes A Kaefer <jak4@gmx.net>
2011-07-28 00:02:54 +0200

    cleanup

--- test/test_statistic_view_config.rb
+++ test/test_statistic_view_config.rb
@@ -4,8 +4,7 @@ class TestStatisticViewConfig < Test::Unit::TestCase
   should "should return view config" do
     fm = FilterModel.new(PARAMS)
     fm.user = USER
-    level = 0
 
-    Qed::Mongodb::StatisticViewConfig.create_config(fm, level)
+    Qed::Mongodb::StatisticViewConfig.create_config(fm)
   end
 end
