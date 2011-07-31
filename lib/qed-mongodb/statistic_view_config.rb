040993dc648a926032353b30e4dd44c04ac010e3
Johannes A Kaefer <jak4@gmx.net>
2011-07-31 02:46:57 +0200

    cleanup two exceptions for wrong input data added

--- lib/qed-mongodb/statistic_view_config.rb
+++ lib/qed-mongodb/statistic_view_config.rb
@@ -3,9 +3,14 @@ module Qed
     class StatisticViewConfig
       # from the config create the necessary objects to perform all mapreduce operations which are needed to generate
       # the desired view/statistic
-      def self.create_config(filter_model)
+      def self.create_config(filter_model, config = Qed::Mongodb::StatisticViewConfigStore::PROFILE)
+        raise Qed::Mongodb::Exceptions::FilterModelError.new("filter_model param ist not a FilterModel-Object!") if !filter_model.is_a?(Qed::Mongodb::FilterModel)
+        raise Qed::Mongodb::Exceptions::FilterModelError.new("config param ist not a hash!") if !config.is_a?(Hash)
+
+
+
         # get the mapreduce-configurations
-        mapreduce_configurations = Qed::Mongodb::StatisticViewConfigStore.get_config(filter_model.user, filter_model.view)[:mapreduce]
+        mapreduce_configurations = get_config(config, filter_model.user, filter_model.view)[:mapreduce]
 
         if( mapreduce_configurations.size > filter_model.drilldown_level_current)
           # only get those needed for the current drilldown level
@@ -23,9 +28,14 @@ module Qed
           end
         # don't mapreduce, just show the filtered data
         else
-          b = Qed::Mongodb::MapReduce::Builder.new({:query => filter_model.mongodb_query })
-          b.database = mapreduce_configurations[0][:database]
+          builder = Qed::Mongodb::MapReduce::Builder.new({:query => filter_model.mongodb_query })
+          builder.database = mapreduce_configurations[0][:database]
+          [builder]
         end
+      end
+
+      def self.get_config(config, user, view)
+        config[user.to_sym][view.to_sym]
       end
     end
   end
