6352f410ea0c990d03ee4b4953eb340d2a2806cf
Johannes A Kaefer <jak4@gmx.net>
2011-07-31 02:50:18 +0200

    parameter handling

--- test/test_statistic_view_config.rb
+++ test/test_statistic_view_config.rb
@@ -1,10 +1,47 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestStatisticViewConfig < Test::Unit::TestCase
-  should "should return view config" do
-    fm = FilterModel.new(PARAMS)
+  should "raise an exception if param filter_model is nil" do
+    assert_raise Qed::Mongodb::Exceptions::FilterModelError do
+      Qed::Mongodb::StatisticViewConfig.create_config(nil, nil)
+    end
+  end
+
+  should "raise an exception if param filter_model is not a filter_model" do
+    assert_raise Qed::Mongodb::Exceptions::FilterModelError do
+      Qed::Mongodb::StatisticViewConfig.create_config([], nil)
+    end
+  end
+
+  should "raise an exception if param mapreduce_config is nil" do
+    fm = FilterModel.new(PARAMS_SCALE_OF_UNIVERSE)
     fm.user = USER
 
-    Qed::Mongodb::StatisticViewConfig.create_config(fm)
+    assert_raise Qed::Mongodb::Exceptions::FilterModelError do
+      Qed::Mongodb::StatisticViewConfig.create_config(fm, nil)
+    end
+  end
+
+  should "raise an exception if param mapreduce_config is not a hash" do
+    fm = FilterModel.new(PARAMS_SCALE_OF_UNIVERSE)
+    fm.user = USER
+
+    assert_raise Qed::Mongodb::Exceptions::FilterModelError do
+      Qed::Mongodb::StatisticViewConfig.create_config(fm, [])
+    end
+  end
+
+  context "creating a config" do
+    setup do
+      fm = FilterModel.new(PARAMS_SCALE_OF_UNIVERSE)
+      fm.user = USER
+    end
+
+    should "should return a valid view config" do
+      fm = FilterModel.new(PARAMS_SCALE_OF_UNIVERSE)
+      fm.user = USER
+
+      Qed::Mongodb::StatisticViewConfig.create_config(fm, MAPREDUCE_CONFIG)
+    end
   end
 end
