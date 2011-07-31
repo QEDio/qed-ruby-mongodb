5672706ff423675d1f7f32cb1b6c006e60f5a497
Johannes A Kaefer <jak4@gmx.net>
2011-07-31 02:49:09 +0200

    refactor

--- test/test_filter_model.rb
+++ test/test_filter_model.rb
@@ -2,21 +2,21 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestFilterModel < Test::Unit::TestCase
   should "should create filter model from params" do
-    fm = FilterModel.new(PARAMS)
+    fm = FilterModel.new(PARAMS_MR2)
 
-    assert_equal ACTION_NAME_VALUE, fm.view
+    assert_equal ACTION_NAME_MR2_VALUE, fm.view
     assert_equal M_S_PRODUCT_NAME_VALUE, fm.filter[:product_name][:value]
   end
 
   should "create mongodb query" do
-    fm = FilterModel.new(PARAMS)
+    fm = FilterModel.new(PARAMS_MR2)
 
     # if you know a better way, please come forward!
     assert_equal PARAMS_RESULTING_MONGODB_QUERY, fm.mongodb_query.to_s
   end
 
   should "convert to json and back again" do
-    fm = FilterModel.new(PARAMS)
+    fm = FilterModel.new(PARAMS_MR2)
     json = fm.to_json
     new_fm = FilterModel.new(json)
 
@@ -24,7 +24,7 @@ class TestFilterModel < Test::Unit::TestCase
   end
 
   should "generate a correct URL with the provided params (next drilldown_level)" do
-    fm = FilterModel.new(PARAMS)
+    fm = FilterModel.new(PARAMS_MR2)
     row = URL_ROW
     key = URL_KEY
     field = URL_FIELD
@@ -33,7 +33,7 @@ class TestFilterModel < Test::Unit::TestCase
   end
 
   should "generate a correct URL with the provided params (next drilldown_level) despite having to clone a symbol" do
-    fm = FilterModel.new(PARAMS)
+    fm = FilterModel.new(PARAMS_MR2)
     fm.user = USER
     row = URL_ROW
     key = URL_KEY
@@ -43,7 +43,7 @@ class TestFilterModel < Test::Unit::TestCase
   end
 
   should "generate a correct URL for itself (same drilldown_level)" do
-    fm = FilterModel.new(PARAMS)
+    fm = FilterModel.new(PARAMS_MR2)
     assert_equal FM_GENERATED_PARAMS_URL, fm.url
   end
 
