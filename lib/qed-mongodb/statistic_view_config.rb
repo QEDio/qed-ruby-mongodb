9208dc4d0fc4fea3bea8a578a157faf3044a2cb2
Johannes A Kaefer <jak4@gmx.net>
2011-10-08 21:45:58 +0200

    cleanup

--- lib/qed-mongodb/statistic_view_config.rb
+++ lib/qed-mongodb/statistic_view_config.rb
@@ -27,46 +27,17 @@ module Qed
             new("Couldn't get mapreduce configuration for user: #{filter_model.user} and view: #{filter_model.view}")
         end
 
-        #if( mapreduce_configurations.size > filter_model.drilldown_level_current)
-          # only get those needed for the current drilldown level
-          # TODO: I guess we have to rethink if a drilldown level of 0 is really the most reduced stats view
-          # drilldown level == 0 is most reduced stats view
-          # drilldown level == size of returned configuration is unreduced stats view (but almost certainly filtered!)
-          #mapreduce_configurations = mapreduce_configurations[0..mapreduce_configurations.size-(1+filter_model.drilldown_level_current)]
-          mapreduce_configuration = mapreduce_configurations[0]
+        mapreduce_configuration = mapreduce_configurations[0]
+        options = {}
 
-          #[].tap do |arr|
-          #  mapreduce_configurations.each_with_index do |int_config, i|
-              # only first mapreduce needs this filter query
-              # interestingly this is exactly the place to implement mapreduce caching
+        if mapreduce_configuration[:time_params]
+          options.merge!({:time_params => mapreduce_configuration[:time_params]})
+        end
 
+        mapreduce_configuration[:query] = Qed::Mongodb::QueryBuilder.selector(filter_model, {:clasz => Qed::Mongodb::MongoidModel}.merge(options))
 
-              options = {}
-
-              if mapreduce_configuration[:time_params]
-                options.merge!({:time_params => mapreduce_configuration[:time_params]})
-              end
-
-              mapreduce_configuration[:query] = Qed::Mongodb::QueryBuilder.selector(filter_model, {:clasz => Qed::Mongodb::MongoidModel}.merge(options))
-
-              # set externally provided map emit keys
-              [set_map_emit_keys(Marbu::MapReduceModel.new(mapreduce_configuration), filter_model)]
-            #end
-          #end
-        # don't mapreduce, just show the filtered data
-        #else
-        #  int_config = mapreduce_configurations.first
-        #  options = {}
-        #  if  int_config[:time_params]
-        #    puts "time_params #{int_config[:time_params]}"
-        #    options.merge!({:time_params => int_config[:time_params]})
-        #  end
-        #  int_config[:query] = Qed::Mongodb::QueryBuilder.selector(filter_model, {:clasz => Qed::Mongodb::MongoidModel}.merge(options))
-        #
-        #  mrm = Marbu::MapReduceModel.new(int_config)
-        #  mrm.force_query = true
-        #  [mrm]
-        #end
+        # set externally provided map emit keys
+        [set_map_emit_keys(Marbu::MapReduceModel.new(mapreduce_configuration), filter_model)]
       end
 
       def self.set_map_emit_keys(mrm, fm)
