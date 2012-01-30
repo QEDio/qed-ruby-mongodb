74668cd53938126c335a796bb4989889eea28fec
Johannes A Kaefer <jak4@gmx.net>
2012-01-30 16:32:40 +0100

    revert finalize naming

--- lib/qed-mongodb/map_reduce/config.rb
+++ lib/qed-mongodb/map_reduce/config.rb
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 module Qed
   module Mongodb
     module MapReduce
@@ -864,23 +863,23 @@ module Qed
 
           :finalize => {
             :values => [
-              {:name => 'Kampagnie',            :function => 'value.campaign_name'},
-              {:name => 'Anzeigengruppe',       :function => 'value.ad_group_name'},
-              {:name => 'Produkt',              :function => 'value.product_name'},
-              {:name => 'Umsatz (€)',           :function => 'Math.round(value.turnover*100)/100'},
-              {:name => 'Adwords Kosten (€)',   :function => 'Math.round(value.cost*100)/100'},
-              {:name => 'Konversions Backend',  :function => 'value.conversions_backend'},
-              {:name => 'Konversions Adwords',  :function => 'value.conversions_adwords'},
-              {:name => 'Bearbeitet',           :function => 'value.worked'},
-              {:name => 'Qualifiziert',         :function => 'value.qualified'},
-              {:name => 'DB (€)',               :function => 'Math.round(db*100)/100'},
-              {:name => 'Rel. DB (%)',          :function => 'Math.round(rel_db*10000)/100'}, #percentage
-              {:name => 'DB2 (€)',              :function => 'Math.round(db2*100)/100'},
-              {:name => 'Ziel CPA (€)',         :function => 'Math.round(target_cpa*100)/100'},
-              {:name => 'Aktueller CPA (€)',    :function => 'Math.round(current_cpa*100)/100'},
-              {:name => 'CR2 (%)',              :function => 'Math.round(cr2*100)/100'},
-              {:name => 'Qual.-Kosten (€)',     :function => 'Math.round(qual_cost*100)/100'},
-              {:name => 'Bezahlt (€)',          :function => 'Math.round(value.payed*100)/100'}
+              {:name => 'campaign_name',          :function => 'value.campaign_name'},
+              {:name => 'ad_group_name',          :function => 'value.ad_group_name'},
+              {:name => 'product_name',           :function => 'value.product_name'},
+              {:name => 'turnover',               :function => 'Math.round(value.turnover*100)/100'},
+              {:name => 'adwords_cost',           :function => 'Math.round(value.cost*100)/100'},
+              {:name => 'conversions_backend',    :function => 'value.conversions_backend'},
+              {:name => 'conversions_adwords',    :function => 'value.conversions_adwords'},
+              {:name => 'worked',                 :function => 'value.worked'},
+              {:name => 'qualified',              :function => 'value.qualified'},
+              {:name => 'db',                     :function => 'Math.round(db*100)/100'},
+              {:name => 'rel_db',                 :function => 'Math.round(rel_db*10000)/100'},
+              {:name => 'db2',                    :function => 'Math.round(db2*100)/100'},
+              {:name => 'target_cpa',             :function => 'Math.round(target_cpa*100)/100'},
+              {:name => 'current_cpa',            :function => 'Math.round(current_cpa*100)/100'},
+              {:name => 'cr2',                    :function => 'Math.round(cr2*100)/100'},
+              {:name => 'qual_cost',              :function => 'Math.round(qual_cost*100)/100'},
+              {:name => 'payed',                  :function => 'Math.round(value.payed*100)/100'}
             ],
 
             :code => {
